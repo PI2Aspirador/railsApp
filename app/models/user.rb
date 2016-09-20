31e866e39f291c988dfb810e6a1d970d2eed24a7
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2016-09-20 23:12:51 +0200

    Adding users to ldap base in create
    Signed-off-by: JohnnysRibeiro <johnnys.ribeiro@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
@@ -8,9 +10,38 @@ class User < ActiveRecord::Base
 def ldap_before_save
   # self.email = Devise::LDAP::Adapter.get_ldap_param(self.name,"mail").first
 end
+
+def save_in_ldap
+  file = File.open("/home/johnny/users.ldif", "w")
+  file.write("dn: uid=" + self.email + ",ou=tech,dc=johnny,dc=com
+objectClass: top
+objectClass: account
+objectClass: posixAccount
+objectClass: shadowAccount
+cn: " + self.email + "
+uid: " + self.email + "
+uidNumber: 20000
+gidNumber: 20000
+homeDirectory: /home/" + self.email + "
+loginShell: /bin/bash
+gecos: " + self.email + ",user,user,435-555-555,801-555-555
+userPassword: " + self.password + "
+shadowLastChange: 0
+shadowMax: 0
+shadowWarning: 0") 
+
+  file.close
+
+  add_ldap = 'ldapadd -x -w "admin" -D "cn=admin,dc=johnny,dc=com" -f /home/johnny/users.ldif'
+  system add_ldap
+
+  File.delete("/home/johnny/users.ldif")
+end
+
 private
   def default_role
     self.roles << Role.find_by_name("user")
 		self.save
+    save_in_ldap
   end
 end
