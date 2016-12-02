a4be07fd06f38ffcd2002d08c4229d7f966a4b67
Ricardo Teixeira <ricardocdzg@hotmail.com>
2016-12-02 17:15:05 -0200

    Fix ldap

---
+++ app/controllers/executer_controller.rb
@@ -0,0 +1,21 @@
+require 'executer.rb'
+class ExecuterController < ApplicationController
+
+
+  def index
+    command = params[:command]
+    script = File.expand_path("execute.sh")
+    puts script
+    puts command
+    case params[:command]
+    when '1'
+      puts "1"
+      system(script+' '+command)
+
+    when '2'
+      system(script+' '+command)
+    else
+      puts "DEU RUIM******************"
+    end
+  end
+end
---
+++ execute.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $1 -eq "1" ]; then
+  echo "Start";
+  exit 0;
+fi
+
+if [ $1 -eq "2" ]; then
+  echo "Stop";
+  exit 0;
+fi
